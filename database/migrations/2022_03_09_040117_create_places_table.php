e78fbde44388bb2f87ef16e7262d3c63579f180f
Shukrullojon <47257193+Shukrullojon@users.noreply.github.com>
2022-03-09 11:15:53 +0500

    Project install

---
+++ database/migrations/2022_03_09_040117_create_places_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_uz');
+            $table->string('name_ru');
+            $table->integer('region_id');
+            $table->string('image')->nullable();
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('places');
+    }
+};
---
+++ database/migrations/2022_03_09_040426_create_fogs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_uz');
+            $table->string('name_ru');
+            $table->integer('region_id');
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fogs');
+    }
+};
